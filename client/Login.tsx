500a04d5d8dcee10d2958cb0d89ff931f2c3df15
Sakelig <71970061+Sakelig@users.noreply.github.com>
2022-06-21 00:06:15 +0200

    inputs for login

--- client/Login.tsx
+++ client/Login.tsx
@@ -1,9 +1,31 @@
 import * as React from "react"
 import {Link} from "react-router-dom";
+import {useState} from "react";
 
 export function Login(){
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log("wow")
+        console.log(username)
+        console.log(password)
+    }
+
+
     return <div>
         Hello!
-        <Link to={"/"}>Back</Link>
+        <div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder={"Username"} type="text"/>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder={"Password"} type="password"/>
+                    <button>Submit
+                    </button>
+                </form>
+            </div>
+            <Link to={"/"}>Back</Link>
+        </div>
     </div>
 }
