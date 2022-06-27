f2a2413d8edfacea4c81d87d09aa31028d7ae993
Sakelig <71970061+Sakelig@users.noreply.github.com>
2022-06-27 18:16:20 +0200

    made stuff for normal user

--- client/Home.tsx
+++ client/Home.tsx
@@ -3,7 +3,9 @@ import {Link, useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
 
 export function Home() {
-
+    const [isOslo, setIsOslo] = useState(false)
+    const [isBergen, setIsBergen] = useState(false)
+    const [isTrondheim, setIsTrondheim] = useState(false)
     const [user, setUser] = useState({})
     let navigate = useNavigate()
 
@@ -22,6 +24,13 @@ export function Home() {
     }
 
 
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(isOslo)
+        console.log(isTrondheim)
+        console.log(isBergen)
+    }
+
     return <div className={"viewContainer"}>
         <div style={{display:"flex", alignContent: "space-between", justifyContent: "space-around"}}>
             <div>Welcome {user.username}!
@@ -32,8 +41,31 @@ export function Home() {
         </div>
         <br/>
         <div className={"centerContent"}>
-            <p>Toggle and click save on the games and areas of the game you want to receive emails about!</p>
+            Current settings:
+            ...
+        </div>
+        <br/>
+        <div className={"centerContent"}>
+            <div>
+                 Toggle and click save on the games and areas of the game you want to receive emails about!
+            </div>
+        </div>
+        <br/>
 
+        <div className={"centerContent"}>
+            <form onSubmit={handleSubmit}>
+                Select the areas you want to get notifications from
+                <div>
+                    <input type="checkbox" id={"Oslo"} name={"Oslo"} checked={isOslo} onChange={() => setIsOslo(!isOslo)}/> Oslo
+                </div>
+                <div>
+                    <input type="checkbox" id={"Bergen"} name={"Bergen"} checked={isBergen} onChange={() => setIsBergen(!isBergen)}/> Bergen
+                </div>
+                <div>
+                    <input type="checkbox" id={"Trondheim"} name={"Trondheim"} checked={isTrondheim} onChange={() => setIsTrondheim(!isTrondheim)}/> Trondheim
+                </div>
+                <button>Save changes</button>
+            </form>
         </div>
     </div>;
 }
